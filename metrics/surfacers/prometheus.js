021adfae20ea5da5933d55580a09a8e8d7f4bf99
dm03514 <dm03514@gmail.com>
2018-12-30 19:39:24 -0500

    WIP Retr

--- metrics/surfacers/prometheus.js
+++ metrics/surfacers/prometheus.js
@@ -1,31 +1,43 @@
+const client = require('prom-client');
+
 
 class Prometheus {
-    constructor(metrics, client) {
+    constructor(metrics, client=null) {
         this.metrics = metrics;
-        this.client = client;
+        this.client = client || {};
     }
 
     surface() {
-        this.metrics.subscribe(this.handleMetric.bind(this, this.metrics, this.client))
+        this.metrics.subscribe(this.handleMetric.bind(this))
     }
 
-    handleMetric(metrics, client, metric) {
+    handleMetric(metric) {
         const metricName = metric.component + '_' + metric.event;
 
         switch (metric.type) {
-            case metrics.type.COUNTER:
-                client[metricName].inc(metric.tags, metric.value);
+            case this.metrics.type.COUNTER:
+                this.client[metricName].inc(metric.tags, metric.value);
                 break;
-            case metrics.type.GAUGE:
-                client[metricName].set(metric.tags, metric.value);
+            case this.metrics.type.GAUGE:
+                this.client[metricName].set(metric.tags, metric.value);
                 break;
-            case metrics.type.HISTOGRAM:
-                client[metricName].observe(metric.tags, metric.value);
+            case this.metrics.type.HISTOGRAM:
+                this.client[metricName].observe(metric.tags, metric.value);
                 break;
             default:
                 throw new Error('not implemented');
         }
     }
+
+    /*
+    client(name, metric) {
+        if (this.client.hasOwnProperty(name)) {
+            return this.client[name]
+        }
+
+        // else lazy instantiate
+    }
+    */
 }
 
 module.exports = {
